5e80d823a5a421829f9bf64f97286f47421f5efc
cachedpotato <chiwonchung98@gmail.com>
2025-02-03 13:10:14 +0900

    from main

--- src/sixth.rs
+++ src/sixth.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, hash::Hash, marker::PhantomData, mem, ptr::NonNull};
+use std::{fmt::Debug, hash::Hash, marker::PhantomData, mem, ops::Add, ptr::NonNull};
 
 //WHAT THE FUCK HAPPENED TO MY BEAUTIFUL CODE
 //WHY GOD WHY IS IT GONE
@@ -14,7 +14,6 @@ pub struct LinkedList<T> {
 }
 
 type Link<T> = Option<NonNull<Node<T>>>;
-
 struct Node<T> {
     front: Link<T>,
     back: Link<T>,
@@ -815,360 +814,170 @@ impl<'a, T> CursorMut<'a, T> {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::LinkedList;
 
-    #[test]
-    fn test_basic() {
-        let mut list = LinkedList::new();
-        // Try to break an empty list
-        assert_eq!(list.len(), 0);
-        assert_eq!(list.pop_front(), None);
-        assert_eq!(list.len(), 0);
+/*                      NEETCODE QUESTIONS                      
+Since Neetcode questions require direct access to Node<T>, It'd be better to just
+solve questions (as in impl functions) here than just make everything public.
+Frankly I don't really care anymore I now join the dark side of hating linked lists.
+I hate linked lists.
 
-        // Try to break a one item list
-        list.push_front(10);
-        assert_eq!(list.len(), 1);
-        assert_eq!(list.pop_front(), Some(10));
-        assert_eq!(list.len(), 0);
-        assert_eq!(list.pop_front(), None);
-        assert_eq!(list.len(), 0);
+"Oh but why not use the stdlib version instead?"
+DOESN'T MATTER IF THE CODE IS ALMOST A DIRECT COPY OF THE BOOK
+I WROTE THIS
+I WILL USE THIS
 
-        // Mess around
-        list.push_front(10);
-        assert_eq!(list.len(), 1);
-        list.push_front(20);
-        assert_eq!(list.len(), 2);
-        list.push_front(30);
-        assert_eq!(list.len(), 3);
-        assert_eq!(list.pop_front(), Some(30));
-        assert_eq!(list.len(), 2);
-        list.push_front(40);
-        assert_eq!(list.len(), 3);
-        assert_eq!(list.pop_front(), Some(40));
-        assert_eq!(list.len(), 2);
-        assert_eq!(list.pop_front(), Some(20));
-        assert_eq!(list.len(), 1);
-        assert_eq!(list.pop_front(), Some(10));
-        assert_eq!(list.len(), 0);
-        assert_eq!(list.pop_front(), None);
-        assert_eq!(list.len(), 0);
-        assert_eq!(list.pop_front(), None);
-        assert_eq!(list.len(), 0);
+The actual question crates will serve as a testing ground to see if the functions
+work without any memory leak/errors.
+*/
+
+//impl blocks per question for convenience
+impl<T> LinkedList<T> {
+    //question 001: reverse list
+    pub fn reverse_list(list: LinkedList<T>) -> LinkedList<T>{
+        LinkedList::from_iter(list.into_iter().rev().collect::<Vec<T>>())
     }
 
-    #[test]
-    fn test_iter() {
-        let mut list = LinkedList::new();
-        list.push_back(1);
-        list.push_back(2);
-        list.push_back(3);
-
-        let mut iter = list.iter();
-        assert_eq!(iter.next(), Some(&1));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&3));
-        assert_eq!(iter.next(), None);
-
-        //IterMut testing with for loops
-        for mut_iter in &mut list {
-            *mut_iter += 1;
+    //Yeah that one is a cop out
+    pub fn reverse(&mut self) {
+        unsafe {
+            let mut curr = self.front;
+            while let Some(node) = curr {
+                let new_front = (*node.as_ptr()).back.take();
+                let new_back = (*node.as_ptr()).front.take();
+                (*node.as_ptr()).front = new_front;
+                (*node.as_ptr()).back = new_back;
+                curr = (*node.as_ptr()).front;
+            }
         }
-        assert_eq!(list.front(), Some(&2));
 
-        //IntoIter testing
-        let mut into_iter = list.into_iter();
-        assert_eq!(into_iter.len(), 3);
-        assert_eq!(into_iter.next(), Some(2));
-        assert_eq!(into_iter.next(), Some(3));
-        assert_eq!(into_iter.next(), Some(4));
-        assert_eq!(into_iter.next(), None);
+        //swap front and back pointers
+        let new_front = self.back.take();
+        let new_back = self.front.take();
+        self.front = new_front;
+        self.back = new_back;
+    }
+}
 
-        //backwards iteration testing
-        let mut list = LinkedList::new();
-        list.push_back(1);
-        list.push_back(2);
-        list.push_back(3);
+impl<T: PartialOrd + PartialEq> LinkedList<T> {
+    pub fn merge_two_lists(mut list1: LinkedList<T>, mut list2: LinkedList<T>) -> LinkedList<T> {
+        let mut out = LinkedList::new();
+
+        while list1.front.is_some() && list2.front.is_some() {
+            unsafe {
+                if (*list1.front.unwrap().as_ptr()).elem <= (*list2.front.unwrap().as_ptr()).elem {
+                    out.push_back(list1.pop_front().unwrap());
+                } else {
+                    out.push_back(list2.pop_front().unwrap());
+                }
+            }
+        }
+
+        //append whatever's left to the end
+        out.extend(list1);
+        out.extend(list2);
+
+        out
+    }
+}
+
+//Let's pretend we can make cyclic lists here
+impl<T> LinkedList<T> {
+    pub fn has_cycle(&mut self) -> bool {
+        let mut set = std::collections::HashSet::new();
+        //I can only have one cursor sooooo....
+        //TODO: IMPLEMENT IMMUTABLE CURSOR
+        let mut mcur = self.cursor_mut();
+        mcur.move_next();
+        while let Some(idx) = mcur.index {
+            if set.contains(&idx) {return true}
+            set.insert(idx);
+            mcur.move_next();
+        }
+        false
+    }
+}
+
+/* 
+impl<T> LinkedList<T> {
+    //TODO: add insert functionality to CursorMut
+    pub fn reorder_linked_list(list: Self) -> Self {
+        unimplemented!()
+    }
+
+    pub fn reorder(&mut self) {
+
+    }
+}
+*/
+
+impl<T> LinkedList<T> {
+    pub fn remove_nth_from_end(mut list: LinkedList<T>, n: usize) -> LinkedList<T> {
+        let mut mcur = list.cursor_mut();
+        for _ in 0..n {
+            mcur.move_prev();
+        }
+        mcur.remove_current();
+        list
+    }
+}
+
+// You know what I know how to solve this
+// I'm just gonna move on
+//impl<T> LinkedList<T> {
+//    pub fn copy_list_with_random_pointer(&self) -> LinkedList<T> {
+//        let mut out = LinkedList::new();
+//        let mut node_map = std::collections::HashMap::<usize, Self>::new();
+//
+//
+//        out
+//
+//
+//    }
+//}
+
+// implementing traits with associated types:
+// <T: trait<type = A>>
+impl<T: Add<Output = T>> LinkedList<T> {
+    pub fn add_two_nodes(mut l1: LinkedList<T>, mut l2: LinkedList<T>) -> LinkedList<T> {
+        let mut out: LinkedList<T> = LinkedList::new();
+
+        // check just l1 because the question's constraint implies
+        // the two lists will always be of the same length
+        while l1.front.is_some() {
+            out.push_back(l1.pop_front().unwrap() + l2.pop_front().unwrap());
+
+        }
+        out
+    }
+}
+
+impl LinkedList<i32> {
+    pub fn add_two_numbers(l1: LinkedList<i32>, l2: LinkedList<i32>) -> LinkedList<i32> {
+
+        if l1.front.is_none() || l2.front.is_none() {
+            return LinkedList::<i32>::new();
+        }
+
+        let n1: i32 = l1.into_iter()
+                        .fold(String::from(""), |acc, x| format!("{}{}", acc, x))
+                        .parse::<i32>().unwrap();
+        let n2: i32 = l2.into_iter()
+                        .fold(String::from(""), |acc, x| format!("{}{}", acc, x))
+                        .parse::<i32>().unwrap();
+
+        let res = format!("{}", n1 + n2).chars()
+            .map(|c| c.to_string().parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        LinkedList::from_iter(res)
+    }
+}
+
+impl<T: Eq> LinkedList<T> {
+    pub fn find_duplicate(list: &mut LinkedList<T>) -> usize {
+        let mcur = list.cursor_mut();
         
-        let mut iter = list.iter();
 
-        assert_eq!(iter.next_back(), Some(&3));
-        assert_eq!(iter.next_back(), Some(&2));
-        assert_eq!(iter.next_back(), Some(&1));
-        assert_eq!(iter.next_back(), None);
-
+        0
     }
-
-    fn generate_test() -> LinkedList<i32> {
-        list_from(&[0, 1, 2, 3, 4, 5, 6])
-    }
-
-    fn list_from<T: Clone>(v: &[T]) -> LinkedList<T> {
-        v.iter().map(|x| (*x).clone()).collect()
-    }
-
-    #[test]
-    fn test_rev_iter() {
-        let m = generate_test();
-        for (i, elt) in m.iter().rev().enumerate() {
-            assert_eq!(6 - i as i32, *elt);
-        }
-        let mut n = LinkedList::new();
-        assert_eq!(n.iter().rev().next(), None);
-        n.push_front(4);
-        let mut it = n.iter().rev();
-        assert_eq!(it.size_hint(), (1, Some(1)));
-        assert_eq!(it.next().unwrap(), &4);
-        assert_eq!(it.size_hint(), (0, Some(0)));
-        assert_eq!(it.next(), None);
-    }
-
-    #[test]
-    fn test_mut_iter() {
-        let mut m = generate_test();
-        let mut len = m.len();
-        for (i, elt) in m.iter_mut().enumerate() {
-            assert_eq!(i as i32, *elt);
-            len -= 1;
-        }
-        assert_eq!(len, 0);
-        let mut n = LinkedList::new();
-        assert!(n.iter_mut().next().is_none());
-        n.push_front(4);
-        n.push_back(5);
-        let mut it = n.iter_mut();
-        assert_eq!(it.size_hint(), (2, Some(2)));
-        assert!(it.next().is_some());
-        assert!(it.next().is_some());
-        assert_eq!(it.size_hint(), (0, Some(0)));
-        assert!(it.next().is_none());
-    }
-
-    #[test]
-    fn test_iterator_mut_double_end() {
-        let mut n = LinkedList::new();
-        assert!(n.iter_mut().next_back().is_none());
-        n.push_front(4);
-        n.push_front(5);
-        n.push_front(6);
-        let mut it = n.iter_mut();
-        assert_eq!(it.size_hint(), (3, Some(3)));
-        assert_eq!(*it.next().unwrap(), 6);
-        assert_eq!(it.size_hint(), (2, Some(2)));
-        assert_eq!(*it.next_back().unwrap(), 4);
-        assert_eq!(it.size_hint(), (1, Some(1)));
-        assert_eq!(*it.next_back().unwrap(), 5);
-        assert!(it.next_back().is_none());
-        assert!(it.next().is_none());
-    }
-
-    #[test]
-    fn test_eq() {
-        let mut n: LinkedList<u8> = list_from(&[]);
-        let mut m = list_from(&[]);
-        assert!(n == m);
-        n.push_front(1);
-        assert!(n != m);
-        m.push_back(1);
-        assert!(n == m);
-
-        let n = list_from(&[2, 3, 4]);
-        let m = list_from(&[1, 2, 3]);
-        assert!(n != m);
-    }
-
-    #[test]
-    fn test_ord() {
-        let n = list_from(&[]);
-        let m = list_from(&[1, 2, 3]);
-        assert!(n < m);
-        assert!(m > n);
-        assert!(n <= n);
-        assert!(n >= n);
-    }
-
-    #[test]
-    fn test_ord_nan() {
-        let nan = 0.0f64 / 0.0;
-        let n = list_from(&[nan]);
-        let m = list_from(&[nan]);
-        assert!(!(n < m));
-        assert!(!(n > m));
-        assert!(!(n <= m));
-        assert!(!(n >= m));
-
-        let n = list_from(&[nan]);
-        let one = list_from(&[1.0f64]);
-        assert!(!(n < one));
-        assert!(!(n > one));
-        assert!(!(n <= one));
-        assert!(!(n >= one));
-
-        let u = list_from(&[1.0f64, 2.0, nan]);
-        let v = list_from(&[1.0f64, 2.0, 3.0]);
-        assert!(!(u < v));
-        assert!(!(u > v));
-        assert!(!(u <= v));
-        assert!(!(u >= v));
-
-        let s = list_from(&[1.0f64, 2.0, 4.0, 2.0]);
-        let t = list_from(&[1.0f64, 2.0, 3.0, 2.0]);
-        assert!(!(s < t));
-        assert!(s > one);
-        assert!(!(s <= one));
-        assert!(s >= one);
-    }
-
-    #[test]
-    fn test_debug() {
-        let list: LinkedList<i32> = (0..10).collect();
-        assert_eq!(format!("{:?}", list), "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]");
-
-        let list: LinkedList<&str> = vec!["just", "one", "test", "more"]
-            .iter().copied()
-            .collect();
-        //the r#""# is there for strings that contiains double quotation marks
-        assert_eq!(format!("{:?}", list), r#"["just", "one", "test", "more"]"#);
-    }
-
-    #[test]
-    fn test_hashmap() {
-        // Check that HashMap works with this as a key
-
-        let list1: LinkedList<i32> = (0..10).collect();
-        let list2: LinkedList<i32> = (1..11).collect();
-        let mut map = std::collections::HashMap::new();
-
-        assert_eq!(map.insert(list1.clone(), "list1"), None);
-        assert_eq!(map.insert(list2.clone(), "list2"), None);
-
-        assert_eq!(map.len(), 2);
-
-        assert_eq!(map.get(&list1), Some(&"list1"));
-        assert_eq!(map.get(&list2), Some(&"list2"));
-
-        assert_eq!(map.remove(&list1), Some("list1"));
-        assert_eq!(map.remove(&list2), Some("list2"));
-
-        assert!(map.is_empty());
-    }
-
-    #[test]
-    fn test_cursor_move_peek() {
-        let mut m: LinkedList<u32> = LinkedList::new();
-        m.extend([1, 2, 3, 4, 5, 6]);
-        let mut cursor = m.cursor_mut();
-        cursor.move_next();
-        assert_eq!(cursor.current(), Some(&mut 1));
-        assert_eq!(cursor.peek_next(), Some(&mut 2));
-        assert_eq!(cursor.peek_prev(), None);
-        assert_eq!(cursor.index(), Some(0));
-        cursor.move_prev();
-        assert_eq!(cursor.current(), None);
-        assert_eq!(cursor.peek_next(), Some(&mut 1));
-        assert_eq!(cursor.peek_prev(), Some(&mut 6));
-        assert_eq!(cursor.index(), None);
-        cursor.move_next();
-        cursor.move_next();
-        assert_eq!(cursor.current(), Some(&mut 2));
-        assert_eq!(cursor.peek_next(), Some(&mut 3));
-        assert_eq!(cursor.peek_prev(), Some(&mut 1));
-        assert_eq!(cursor.index(), Some(1));
-
-        let mut cursor = m.cursor_mut();
-        cursor.move_prev();
-        assert_eq!(cursor.current(), Some(&mut 6));
-        assert_eq!(cursor.peek_next(), None);
-        assert_eq!(cursor.peek_prev(), Some(&mut 5));
-        assert_eq!(cursor.index(), Some(5));
-        cursor.move_next();
-        assert_eq!(cursor.current(), None);
-        assert_eq!(cursor.peek_next(), Some(&mut 1));
-        assert_eq!(cursor.peek_prev(), Some(&mut 6));
-        assert_eq!(cursor.index(), None);
-        cursor.move_prev();
-        cursor.move_prev();
-        assert_eq!(cursor.current(), Some(&mut 5));
-        assert_eq!(cursor.peek_next(), Some(&mut 6));
-        assert_eq!(cursor.peek_prev(), Some(&mut 4));
-        assert_eq!(cursor.index(), Some(4));
-    }
-
-    #[test]
-    fn test_cursor_mut_insert() {
-        let mut m: LinkedList<u32> = LinkedList::new();
-        m.extend([1, 2, 3, 4, 5, 6]);
-        let mut cursor = m.cursor_mut();
-        cursor.move_next();
-        cursor.splice_before(Some(7).into_iter().collect());
-        cursor.splice_after(Some(8).into_iter().collect());
-        // check_links(&m);
-        assert_eq!(m.iter().cloned().collect::<Vec<_>>(), &[7, 1, 8, 2, 3, 4, 5, 6]);
-        let mut cursor = m.cursor_mut();
-        cursor.move_next();
-        cursor.move_prev();
-        cursor.splice_before(Some(9).into_iter().collect());
-        cursor.splice_after(Some(10).into_iter().collect());
-        check_links(&m);
-        assert_eq!(m.iter().cloned().collect::<Vec<_>>(), &[10, 7, 1, 8, 2, 3, 4, 5, 6, 9]);
-        
-        let mut cursor = m.cursor_mut();
-        cursor.move_next();
-        cursor.move_prev();
-        assert_eq!(cursor.remove_current(), None);
-        cursor.move_next();
-        cursor.move_next();
-        assert_eq!(cursor.remove_current(), Some(7));
-        cursor.move_prev();
-        cursor.move_prev();
-        cursor.move_prev();
-        assert_eq!(cursor.remove_current(), Some(9));
-        cursor.move_next();
-        assert_eq!(cursor.remove_current(), Some(10));
-        check_links(&m);
-        assert_eq!(m.iter().cloned().collect::<Vec<_>>(), &[1, 8, 2, 3, 4, 5, 6]);
-
-        let mut m: LinkedList<u32> = LinkedList::new();
-        m.extend([1, 8, 2, 3, 4, 5, 6]);
-        let mut cursor = m.cursor_mut();
-        cursor.move_next();
-        let mut p: LinkedList<u32> = LinkedList::new();
-        p.extend([100, 101, 102, 103]);
-        let mut q: LinkedList<u32> = LinkedList::new();
-        q.extend([200, 201, 202, 203]);
-        cursor.splice_after(p);
-        cursor.splice_before(q);
-        check_links(&m);
-        assert_eq!(
-            m.iter().cloned().collect::<Vec<_>>(),
-            &[200, 201, 202, 203, 1, 100, 101, 102, 103, 8, 2, 3, 4, 5, 6]
-        );
-        let mut cursor = m.cursor_mut();
-        cursor.move_next();
-        cursor.move_prev();
-        let tmp = cursor.split_before();
-        assert_eq!(m.into_iter().collect::<Vec<_>>(), &[]);
-        m = tmp;
-        let mut cursor = m.cursor_mut();
-        cursor.move_next();
-        cursor.move_next();
-        cursor.move_next();
-        cursor.move_next();
-        cursor.move_next();
-        cursor.move_next();
-        cursor.move_next();
-        let tmp = cursor.split_after();
-        assert_eq!(tmp.into_iter().collect::<Vec<_>>(), &[102, 103, 8, 2, 3, 4, 5, 6]);
-        check_links(&m);
-        assert_eq!(m.iter().cloned().collect::<Vec<_>>(), &[200, 201, 202, 203, 1, 100, 101]);
-    }
-
-    fn check_links<T: Eq + std::fmt::Debug>(list: &LinkedList<T>) {
-        let from_front: Vec<_> = list.iter().collect();
-        let from_back: Vec<_> = list.iter().rev().collect();
-        let re_reved: Vec<_> = from_back.into_iter().rev().collect();
-        assert_eq!(from_front, re_reved);
-    }
-
 }
